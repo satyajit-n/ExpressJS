49dccc329e3f7fed00dfb67d3fa5f827f97d0d97
satyajit-n <namawar.satyajeet@gmail.com>
2023-03-23 23:08:26 +0530

    Custom response COMMIT

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log('Satyajeet Namawar');
+  console.log("Satyajeet Namawar");
+  console.log(req.url);
+  if (req.url == "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>First Page</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>First Page</title></head>");
+    res.write("<body><h1>About US</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (req.url == "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>First Page</title></head>");
+    res.write("<body><h1>Welcome TO node js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(4000);
