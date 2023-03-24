b834b47c8448abaa63028f25d6c2cd72fb5d783f
satyajit-n <namawar.satyajeet@gmail.com>
2023-03-24 15:18:50 +0530

    CHAT APP COMMIT

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Satyajeet Namawar");
-  console.log(req.url);
-  if (req.url == "/home") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>First Page</title></head>");
-    res.write("<body><h1>Welcome Home</h1></body>");
-    res.write("</html>");
-    res.end();
-  } else if (req.url == "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>First Page</title></head>");
-    res.write("<body><h1>About US</h1></body>");
-    res.write("</html>");
-    res.end();
-  } else if (req.url == "/node") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>First Page</title></head>");
-    res.write("<body><h1>Welcome TO node js project</h1></body>");
-    res.write("</html>");
-    res.end();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>First Page</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1].replaceAll("+", " ");
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>First Page</title></head>");
+  // res.write("<body><h1>HELLO</h1></body>");
+  // res.write("</html>");
+  // res.end();
 });
 
 server.listen(4000);
