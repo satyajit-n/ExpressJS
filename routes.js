b37e87018b908c21f10b08a00c6b58a0b0c3ca9e
satyajit-n <namawar.satyajeet@gmail.com>
2023-03-25 09:59:43 +0530

    Export first way COMMIT

---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>First Page</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1].replaceAll("+", " ");
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<html>");
+  // res.write("<head><title>First Page</title></head>");
+  // res.write("<body><h1>HELLO</h1></body>");
+  // res.write("</html>");
+  // res.end();
+};
+
+module.exports = requestHandler;
