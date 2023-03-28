53399d2f496fbda622a5c146a67f2fcbac2b6ea7
satyajit-n <namawar.satyajeet@gmail.com>
2023-03-28 16:39:21 +0530

    Intro to Middlewares COMMIT

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In form middleware");
+  res.send(
+    '<h1>Add Product</h1><form action="/product" method="POST"><input type="text" name="title" placeholder="Product Name"><input type="text" name="size" placeholder="Product size"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
-  res.send({ key1: "value" });
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express js</h1>");
 });
 
 app.listen(4000);
